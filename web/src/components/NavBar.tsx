29dbeb2c39f5fb55999ec99ed672c3b9e2627b68
Adam Kamboj <kamboj.adam@gmail.com>
2021-06-04 01:08:45 -0400

    added createpost functionality

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -38,7 +38,7 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
     );
   }
   return (
-    <Flex bg="tomato" p={5}>
+    <Flex zIndex={1} position="sticky" top="0" bg="tomato" p={5}>
       <Box ml={"auto"}>{body}</Box>
     </Flex>
   );
